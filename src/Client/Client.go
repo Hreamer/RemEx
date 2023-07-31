e21252519bc86af93dd8f7b6aabd02a9f29b613e
HReamer <reamer.hudson@gmail.com>
2023-07-30 21:33:23 -0500

    Made --help flag for Client.go

--- src/Client/Client.go
+++ src/Client/Client.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
+
+/*
+	Tasks:
+		Task 1: Use flags library to make Help flag [X]
+		Task 2: Get File via args [X]
+		Task 3: Open the File []
+		Task 4: Send the file []
+		Task 5: Wait for the return []
+		Task 6: Grab the return []
+		Task 7: Print the return []
+
+*/
 
 func main() {
-	fmt.Println("Setup the Client Folder")
+	//This will Parse the flags and also print help message for all flags if -help is called
+	var path = flag.String("PATH", "", "The path of the file you wish to send")
+	flag.Parse()
+
+	fmt.Printf("-PATH was set to: %s\n", *path)
+
 }
