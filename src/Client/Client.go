bd80a8da0a65bd9f45f5a47e128a574e02f0d125
HReamer <reamer.hudson@gmail.com>
2023-08-06 23:33:13 -0500

    First Draft of Server.go and Client.go

--- src/Client/Client.go
+++ src/Client/Client.go
@@ -3,13 +3,16 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
 )
 
 /*
 	Tasks:
 		Task 1: Use flags library to make Help flag [X]
 		Task 2: Get File via args [X]
-		Task 3: Open the File []
+		Task 3: Open the File [X]
 		Task 4: Send the file []
 		Task 5: Wait for the return []
 		Task 6: Grab the return []
@@ -18,10 +21,41 @@ import (
 */
 
 func main() {
-	//This will Parse the flags and also print help message for all flags if -help is called
-	var path = flag.String("PATH", "", "The path of the file you wish to send")
+	//Parse the flags and also print help message for all flags if -help is called
+	var path = flag.String("p", "", "The path of the file you wish to send")
+	var address = flag.String("a", "", "The address to send the file")
+	var language = flag.String("t", "", "The language of the file. Check README for supported languages")
+
 	flag.Parse()
 
 	fmt.Printf("-PATH was set to: %s\n", *path)
+	fmt.Printf("-Address was set to %s\n", *address)
 
+	//open the file
+	file, err := os.Open(*path)
+	if err != nil {
+		fmt.Printf("RemEx could not open file in path %s\n", *path)
+		os.Exit(0)
+	}
+
+	//send the file
+	//Note: os.Open returns a file type and it implements the io.Reader interface
+	resp, postErr := http.Post(*address+"/"+*language, "text/plain", file)
+	if postErr != nil {
+		fmt.Println("RemEx failed to send the POST request")
+		os.Exit(0)
+	}
+
+	//Get the Body as a string slice so we can convert it to string to print
+	body, ioerr := io.ReadAll(resp.Body)
+	if ioerr != nil {
+		fmt.Println("Could not read the body of the response")
+		os.Exit(0)
+	}
+	resp.Body.Close()
+
+	//Print reponse
+	fmt.Printf("Server Response:\n%s\n", string(body))
+
+	file.Close()
 }
--- src/Server/Server.go
+++ src/Server/Server.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+)
 
 func main() {
-	fmt.Println("Setup the server folder")
+	//Set up handlers
+	http.HandleFunc("/go", goHandler)
+
+	//Start listening for requests
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func goHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/go" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "POST" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+
+	//read file contents
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Could Not Read Body", http.StatusNotFound)
+		return
+	}
+
+	//Create file to store the contents of the POST request
+	//NOTE: If test.go exists it will delete the contents of this file so no worries
+	file, createErr := os.Create("test.go")
+	if createErr != nil {
+		http.Error(w, "Could Not Open File", http.StatusNotFound)
+		return
+	}
+
+	//Now write the contents to the file
+	file.Write(body)
+	file.Close()
+
+	cmd := exec.Command("go", "build", file.Name())
+	stdout, err := cmd.Output()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, string(stdout))
 }
