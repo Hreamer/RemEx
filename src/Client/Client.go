1a90f03a5effa72597bda5b2977d4e44ab26e12b
HReamer <reamer.hudson@gmail.com>
2023-08-07 00:46:41 -0500

    Cleaned up TODO comment

--- src/Client/Client.go
+++ src/Client/Client.go
@@ -8,18 +8,6 @@ import (
 	"os"
 )
 
-/*
-	Tasks:
-		Task 1: Use flags library to make Help flag [X]
-		Task 2: Get File via args [X]
-		Task 3: Open the File [X]
-		Task 4: Send the file []
-		Task 5: Wait for the return []
-		Task 6: Grab the return []
-		Task 7: Print the return []
-
-*/
-
 func main() {
 	//Parse the flags and also print help message for all flags if -help is called
 	var path = flag.String("p", "", "The path of the file you wish to send")
